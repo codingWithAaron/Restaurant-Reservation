75764ab65a1f22256a69b74b6134a7aa9a693d29
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-08-07 10:09:57 -0600

    initial commit

---
+++ back-end/src/reservations/reservations.controller.js
@@ -0,0 +1,14 @@
+const service = require("./reservations.service")
+
+/**
+ * List handler for reservation resources
+ */
+async function list(req, res) {
+  res.json({
+    data: [],
+  });
+}
+
+module.exports = {
+  list,
+};
