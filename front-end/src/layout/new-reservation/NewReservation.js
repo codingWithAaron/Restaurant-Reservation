4c2fc26a6db595e1b9b57afd1260fc28ae1268b2
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-08-09 08:53:52 -0600

    Added alerts to NewReservation to show if the reservation_time is not valid

--- front-end/src/layout/new-reservation/NewReservation.js
+++ front-end/src/layout/new-reservation/NewReservation.js
@@ -15,6 +15,8 @@ function NewReservation(){
     const [formData, setFormData] = useState(initialFormData)
     const [isTuesday, setIsTuesday] = useState(false)
     const [isPastDate, setIsPastDate] = useState(false)
+    const [before1030, setBefore1030] = useState(false)
+    const [after930, setAfter930] = useState(false)
 
     function isDateTuesday(date){
         const selectedDate = new Date(`${date}T00:00:00`)
@@ -32,6 +34,18 @@ function NewReservation(){
         return selectedDate < currentDate
     }
 
+    function isBefore10(time){
+        const selectedTime = new Date(`1970-01-01T${time}`)
+        const earliestTime = new Date(`1970-01-01T10:30:00`)
+        return selectedTime < earliestTime
+    }
+
+    function isAfter9(time){
+        const selectedTime = new Date(`1970-01-01T${time}`)
+        const latestTime = new Date(`1970-01-01T21:30:00`)
+        return selectedTime > latestTime
+    }
+
     function handleCancel(){
      history.goBack()
     }
@@ -47,6 +61,18 @@ function NewReservation(){
                 setIsPastDate(true)
             }else{
                 setIsPastDate(false)
+            }
+        }else if(event.target.name === "reservation_time"){
+            if(isBefore10(event.target.value)){
+                setBefore1030(true)
+            }else{
+                setBefore1030(false)
+            }
+
+            if(isAfter9(event.target.value)){
+                setAfter930(true)
+            }else{
+                setAfter930(false)
             }
         }
     }
@@ -85,6 +111,8 @@ function NewReservation(){
                     {isTuesday && !isPastDate ? <div className="alert alert-danger"><p>The restaurant is closed on Tuesdays. Please choose another day.</p></div> : ""}
                     {isPastDate && !isTuesday ? <div className="alert alert-danger"><p>You picked a date that is in the past. Please choose a different date.</p></div> : ""}
                     {isTuesday && isPastDate ? <div className="alert alert-danger"><p>The restaurant is closed on Tuesdays. Please choose another day.</p> <p>You also picked a date that is in the past. Please choose a different date.</p></div> : ""}
+                    {before1030 ? <div className="alert alert-danger"><p>Please choose a time after 10:30 AM.</p></div> : ""}
+                    {after930 ? <div className="alert alert-danger"><p>Please choose a time before 9:30 PM.</p></div> : ""}
                 </form>
             </div>
         </>
