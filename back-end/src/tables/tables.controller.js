6947ab4ffbd1af90a1b1d32cb194ce6f0c4a126e
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-08-15 09:48:41 -0600

    validators for update /tables

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,6 +1,25 @@
 const service = require("./tables.service")
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
 
+async function validateTableisOccupied(req, res, next){
+    const { table_id } = req.body.data
+    const data = await service.read(table_id)
+
+    if(data.reservation_id === null){
+        next()
+    }else{
+        return next({status: 400, message: "Table is occupied."})
+    }
+}
+
+function validateHasData(req, res, next){
+    const data = req.body.data
+    if(data){
+        next()
+    }else{
+        return next({status: 400, message: "Must contain data in the body."})
+    }
+}
 
 async function list(req, res, _next){
     const data = await service.list()
@@ -11,6 +30,7 @@ async function update(req, res, _next){
  const reservationSeat = {
     ...req.body.data
  }
+ console.log("This is the reservation seat ############", reservationSeat)
  const data = await service.update(reservationSeat)
  res.json({data})
 }
@@ -20,8 +40,13 @@ async function create(req, res, _next){
     res.status(201).json({data})
 }
 
+
 module.exports = {
     list: [asyncErrorBoundary(list)],
-    update: [asyncErrorBoundary(update)],
+    update: [
+        validateHasData,
+        asyncErrorBoundary(validateTableisOccupied),
+        asyncErrorBoundary(update)
+    ],
     create: [asyncErrorBoundary(create)],
 }
