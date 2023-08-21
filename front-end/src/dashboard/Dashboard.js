017230e7b9b916f58e0ab048149016f7eda90663
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-08-21 09:55:39 -0600

    lifted up the queryDate to the Routes component & cleaned up the code in Dashboard. Also updated the migrations table for reservations to have the status defaultTo booked

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
-import useQuery from "../utils/useQuery";
-import { useRouteMatch } from "react-router-dom/cjs/react-router-dom";
 import { today, previous, next } from "../utils/date-time";
 import EachReservation from "../reservations/EachReservation";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
@@ -15,37 +13,23 @@ import EachTable from "../tables/EachTable";
  * @returns {JSX.Element}
  */
 function Dashboard({ date }) {
-  const query = useQuery();
-  const route = useRouteMatch();
   const history = useHistory()
 
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
-  const [currentDate, setCurrentDate] = useState(date);
   const [tables, setTables] = useState([])
 
-  useEffect(loadDashboard, [currentDate]);
+  useEffect(loadDashboard, [date]);
 
   function loadDashboard() {
     const abortController = new AbortController();
     setReservationsError(null);
-    listReservations({ date: currentDate }, abortController.signal)
+    console.log(date)
+    listReservations({ date: date }, abortController.signal)
       .then(setReservations)
       .catch(setReservationsError);
     return () => abortController.abort();
   }
-
-  useEffect(() => {
-    function getDate() {
-      const queryDate = query.get("date");
-      if (queryDate) {
-        setCurrentDate(queryDate);
-      } else {
-        setCurrentDate(today());
-      }
-    }
-    getDate();
-  }, [query, route]);
 
   useEffect(()=>{
     async function getTables(){
@@ -61,29 +45,26 @@ function Dashboard({ date }) {
       <main>
         <h1>Dashboard</h1>
         <div className="d-md-flex mb-3">
-          <h4 className="mb-0">Reservations for date: {currentDate}</h4>
+          <h4 className="mb-0">Reservations for date: {date}</h4>
           <button
             className="btn btn-secondary ml-2"
             type="button"
             onClick={() => {
-              history.push(`/dashboard?date=${previous(currentDate)}`);
-              setCurrentDate(previous(currentDate));
+              history.push(`/dashboard?date=${previous(date)}`);
             }}
           >Previous Day</button>
           <button
             className="btn btn-primary ml-2"
             type="button"
             onClick={() => {
-              history.push(`/dashboard?date=${today(currentDate)}`);
-              setCurrentDate(today(currentDate));
+              history.push(`/dashboard?date=${today()}`);
             }}
           >Today</button>
           <button
             className="btn btn-secondary ml-2"
             type="button"
             onClick={() => {
-              history.push(`/dashboard?date=${next(currentDate)}`);
-              setCurrentDate(next(currentDate));
+              history.push(`/dashboard?date=${next(date)}`);
             }}
           >Next Day</button>
         </div>
@@ -109,29 +90,26 @@ function Dashboard({ date }) {
       <main>
         <h1>Dashboard</h1>
          <div className="d-md-flex mb-3">
-          <h4 className="mb-0">No Reservations for date: {currentDate}</h4>
+          <h4 className="mb-0">No Reservations for date: {date}</h4>
           <button
             className="btn btn-secondary ml-2"
             type="button"
             onClick={() => {
-              history.push(`/dashboard?date=${previous(currentDate)}`);
-              setCurrentDate(previous(currentDate));
+              history.push(`/dashboard?date=${previous(date)}`);
             }}
           >Previous Day</button>
           <button
             className="btn btn-primary ml-2"
             type="button"
             onClick={() => {
-              history.push(`/dashboard?date=${today(currentDate)}`);
-              setCurrentDate(today(currentDate));
+              history.push(`/dashboard?date=${today()}`);
             }}
           >Today</button>
           <button
             className="btn btn-secondary ml-2"
             type="button"
             onClick={() => {
-              history.push(`/dashboard?date=${next(currentDate)}`);
-              setCurrentDate(next(currentDate));
+              history.push(`/dashboard?date=${next(date)}`);
             }}
           >Next Day</button>
         </div>
--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
+import useQuery from "../utils/useQuery";
 import { today } from "../utils/date-time";
 import CreateReservation from "../reservations/CreateReservation";
 import CreateTable from "../tables/CreateTable";
@@ -16,6 +17,8 @@ import SearchReservation from "../reservations/SearchReservation";
  * @returns {JSX.Element}
  */
 function Routes() {
+  const query = useQuery();
+  const queryDate = query.get("date") || today();
   return (
     <Switch>
       <Route exact={true} path="/">
@@ -37,7 +40,7 @@ function Routes() {
         <SearchReservation />
       </Route>
       <Route path="/dashboard">
-        <Dashboard date={today()} />
+        <Dashboard date={queryDate} />
       </Route>
       <Route>
         <NotFound />
